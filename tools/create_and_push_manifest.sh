711589776c8b1e170b6e8dadf4b69290bb192538
Kevin Reis <kreis-dev@gmx.net>
2023-05-06 09:56:58 +0000

    Add create_and_push_manifest.sh

---
+++ tools/create_and_push_manifest.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+if [[ "$#" -ne 2 ]]; then
+    echo "Illegal number of arguments, need 2 arguments: <output of docker/metadata-action> <desired Docker image name>" >&2
+    exit 1
+fi
+
+readonly __docker_image_name="$2"
+
+TAGS=()
+NEW_IMAGE_NAMES=()
+LOCAL_IMAGE_NAMES=()
+while IFS=','; read -ra LOCAL_TAGS; do
+    for i in "${LOCAL_TAGS[@]}"; do
+        __tag=$(cut -d ':' -f3 <<< "$i")
+        LOCAL_IMAGE_NAMES+=("$i")
+        NEW_IMAGE_NAMES+=("$__docker_image_name:$__tag")
+        TAGS+=("$__tag")
+    done
+done <<< "$1"
+
+for image_name in "${NEW_IMAGE_NAMES[@]}"; do
+    docker buildx imagetools create -t "$image_name" ${LOCAL_IMAGE_NAMES[@]} localhost:5000/traefik-certs-dumper:armhf
+done
+
+exit 0
